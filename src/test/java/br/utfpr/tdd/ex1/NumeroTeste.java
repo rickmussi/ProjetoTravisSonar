8f54c4f76fd0d36f1fde6e71c4a9111cba564d93
rickmussi <39230898+rickmussi@users.noreply.github.com>
2018-05-19 01:11:56 -0300

    Add files via upload

---
+++ src/test/java/br/utfpr/tdd/ex1/NumeroTeste.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.utfpr.tdd.ex1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mussi
+ */
+public class NumeroTeste {
+    
+    public NumeroTeste() {
+    }
+
+     @Test
+     public void vetorNmero() {
+     int[] n = {5,8,9,10,12};
+     
+     }
+}
